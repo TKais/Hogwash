c004400e9a6c4cc3f23fe9a22486fcc1523234b7
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-04-25 19:13:49 -0700

    Move html into 'views' folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-          template: "./src/client/index.html",
+          template: "./src/client/views/index.html",
           filename: "./index.html",
         }),
         new CleanWebpackPlugin({
