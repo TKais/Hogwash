7b0d7ff558f67b2903eaeb03a20fbd4563bbf476
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-05-15 06:46:12 -0700

    Add test for Search within App component

--- test/App.test.js
+++ test/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import App from '../src/client/components/App.jsx';
 import Header from '../src/client/components/Header.jsx';
+import Search from '../src/client/components/Search.jsx';
 
 describe('App', () => {
   const appComponent = shallow(<App />);
@@ -12,5 +13,8 @@ describe('App', () => {
   });
   test('should have header component', () => {
     expect(appComponent.find(Header)).toHaveLength(1);
+  });
+  test('should have a Search component', () => {
+    expect(appComponent.find(Search)).toHaveLength(1);
   });
 })
