fd1ed9361fbd836febea5debb7d1c5c8f3999c77
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-05-19 00:03:51 -0700

    Add dependencies to allow async/await to be used and prevent XSS attacks

--- src/client/js/getArticles.js
+++ src/client/js/getArticles.js
@@ -1,12 +1,20 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// These packages are required to use async/await
+// Babel docs recommend this over @babel/polyfill for @babel versions 7 or later
+// https://babeljs.io/docs/en/babel-polyfill
+
 /*
 // Implementation details
 */
 
 function isValidQueryString(queryString) {
-  console.log(queryString);
   // Run logic to prevent XSS attacks
+  if (queryString && /[<>&]/.test(queryString)) {
+    return false;
+  }
+  return true;
 }
-
 
 /*
 // API
