220cc4791949f680c5509d3940adaa1c8731dc0f
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-05-26 19:20:55 -0700

    Fix List's layout of data

--- src/client/components/List.jsx
+++ src/client/components/List.jsx
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 
 function List({ items }) {
   return (
-    <ul>
+    <ul className="list">
       { items.map((item, index) => (
-        <li key={`article-${item.source.id}-${index}`}>{item.title}</li>
+        <li key={`article-${item.source.id}-${index}`} className="list__item">
+          <a href={item.url}>
+            {new Date(item.publishedAt).toLocaleDateString()}
+            {item.source.name}
+            {item.title}
+          </a>
+        </li>
       ))
       }
     </ul>
