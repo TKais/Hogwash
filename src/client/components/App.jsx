8eba9324761c5a7ddf829e54a3d2f8f1da63b632
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-05-30 18:13:23 -0700

    Add theme class switching in App component

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header.jsx';
 import Search from './Search.jsx';
+import Toggle from './Toggle.jsx';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  function handleThemeChange() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
+  useEffect(() => {
+    const bodyClasses = document.body.classList;
+    if (bodyClasses.value) {
+      bodyClasses.replace(bodyClasses.value, theme);
+    } else {
+      document.body.classList.add(theme);
+    }
+  }, [theme]);
+
   return (
     <div>
       <Header headerText="Hogwash" />
       <Search />
+      <Toggle onClick={handleThemeChange} />
     </div>
   );
 }
